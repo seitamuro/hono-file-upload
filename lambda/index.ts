39027c96e75dfe37679a4f033704def95310c841
seitamuro <seitamuro0210@gmail.com>
2024-08-29 00:07:45 +0900

    s3にファイルをアップロード

--- lambda/index.ts
+++ lambda/index.ts
@@ -1,13 +1,29 @@
+import { S3Client } from "@aws-sdk/client-s3";
+import { HonoS3Storage } from "@hono-storage/s3";
 import { Hono } from "hono";
 import { handle } from "hono/aws-lambda";
 
 const app = new Hono();
 
-app.post("/upload", async (c) => {
-  const parseBody = await c.req.parseBody();
-  console.log(parseBody);
-  const formData = await c.req.formData();
-  console.log(formData);
+const client = new S3Client({ region: "us-east-1" });
+
+const BUCKET_NAME = process.env.BUCKET_NAME;
+if (!BUCKET_NAME) {
+  throw new Error("BUCKET_NAME is required");
+}
+
+const storage = new HonoS3Storage({
+  key: (_, file) => `${file.originalname}-${Date.now()}.${file.extension}`,
+  bucket: BUCKET_NAME,
+  client: client,
+});
+
+app.post("/upload", storage.single("file"), async (c) => {
+  const { file } = c.var.files;
+  if (file) {
+    return c.json({ message: "File uploaded successfully", file });
+  }
+  return c.json({ message: "Failed to upload the file" }, 400);
 });
 
 export const handler = handle(app);
--- lib/hono-file-upload-stack.ts
+++ lib/hono-file-upload-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as s3 from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -8,11 +9,20 @@ export class HonoFileUploadStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const bucket = new s3.Bucket(this, "Bucket", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      bucketName: "hono-file-upload-bucket-seimiura",
+    });
+
     const fn = new NodejsFunction(this, "HonoFileUploadHandler", {
       entry: "lambda/index.ts",
       handler: "handler",
       runtime: lambda.Runtime.NODEJS_20_X,
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+      },
     });
+    bucket.grantPut(fn);
     const fnUrl = fn.addFunctionUrl({
       authType: lambda.FunctionUrlAuthType.NONE,
     });
