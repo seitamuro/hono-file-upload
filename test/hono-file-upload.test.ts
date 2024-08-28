304589040bbf16c8326354dc9ffc7a491bbb947c
seitamuro <seitamuro0210@gmail.com>
2024-08-28 23:39:37 +0900

    Initial commit

---
+++ test/hono-file-upload.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HonoFileUpload from '../lib/hono-file-upload-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hono-file-upload-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HonoFileUpload.HonoFileUploadStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
